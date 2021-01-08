e9a03308089af4c4584f22267faf972bb0fcff19
Development <eric@bottlerocket.design>
2021-01-08 17:36:30 -0500

    unified version naming

--- emma-slider.php
+++ emma-slider.php
@@ -15,13 +15,15 @@ Text Domain: emma_slider
  */
 function emma_slider_enqueue_frontend() {
   $slider_library_js = plugin_dir_url( __FILE__ ) . 'vendor/glide/glide.min.js';
-  $slider_frontend_js = plugin_dir_url( __FILE__ ) . 'js/slider-frontend.js';;
-  $slider_version = '3.4.1';
+  $slider_library_version = '3.4.1';
+  
+  $slider_frontend_js = plugin_dir_url( __FILE__ ) . 'js/slider-frontend.js';
+  $slider_frontend_version = '1.0.0';
 
   global $post;
   if( has_block( 'emma/slider', $post ) ) {
-    wp_enqueue_script( 'slider-library', $slider_library_js, [], $slider_version, true );
-    wp_enqueue_script( 'slider-frontend', $slider_frontend_js, ['slider-library'], $slider_version, true );
+    wp_enqueue_script( 'slider-library', $slider_library_js, [], $slider_library_version, true );
+    wp_enqueue_script( 'slider-frontend', $slider_frontend_js, ['slider-library'], $slider_frontend_version, true );
   }
 }
 add_action( 'wp_enqueue_scripts', 'emma_slider_enqueue_frontend' );
@@ -31,8 +33,8 @@ add_action( 'wp_enqueue_scripts', 'emma_slider_enqueue_frontend' );
  */
 function emma_slider_enqueue_editor() {
   $slider_editor_js = plugin_dir_url( __FILE__ ) . 'js/slider-editor.js';
-  $slider_block_version = '1.0.0';
+  $slider_editor_version = '1.0.0';
 
-  wp_enqueue_script( 'slider-editor', $slider_editor_js, ['wp-blocks','wp-editor'], $slider_block_version, true );
+  wp_enqueue_script( 'slider-editor', $slider_editor_js, ['wp-blocks','wp-editor'], $slider_editor_version, true );
 }
 add_action( 'enqueue_block_editor_assets', 'emma_slider_enqueue_editor' );
