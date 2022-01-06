cc30ded5f905e62df7e6cb2ceeebfe03e3820e9d
D.S. Webster <dswebs.me@gmail.com>
2022-01-06 06:41:06 -0500

    Code cleanup

--- emma-slider.php
+++ emma-slider.php
@@ -1,33 +1,33 @@
 <?php
-/*
-Plugin Name: Emma Slider
-Plugin URI: https://github.com/pressden/emma-slider
-Description: Emma Slider is a WordPress plugin that adds a complex, configurable slider block to the Gutenberg editor for the Emma theme.
-Version: 1.0.0
-Author: Eric Michel
-Author URI: http://pressden.com/
-License: GPLv2 or later
-Text Domain: emma_slider
-*/
+/**
+ * Plugin Name: Emma Slider
+ * Plugin URI: https://github.com/pressden/emma-slider
+ * Description: Emma Slider is a WordPress plugin that adds a complex, configurable slider block to the Gutenberg editor for the Emma theme.
+ * Version: 1.0.0
+ * Author: Eric Michel
+ * Author URI: http://pressden.com/
+ * License: GPLv2 or later
+ * Text Domain: emma_slider
+ *
+ * @package Emma_Slider
+ */
 
 /**
  * Register and conditionally enqueue frontend scripts
  */
 function emma_slider_enqueue_frontend() {
-  $slider_library_js = plugin_dir_url( __FILE__ ) . 'vendor/splide/js/splide.min.js';
-  $slider_library_css = plugin_dir_url( __FILE__ ) . 'vendor/splide/css/splide-core.min.css';
-  $slider_library_version = '2.4.21';
-  
-  $slider_frontend_js = plugin_dir_url( __FILE__ ) . 'js/slider-frontend.js';
-  $slider_frontend_version = '1.0.0';
+	global $post;
+	$slider_library_js = plugin_dir_url( __FILE__ ) . 'vendor/splide/js/splide.min.js';
+	$slider_library_css = plugin_dir_url( __FILE__ ) . 'vendor/splide/css/splide-core.min.css';
+	$slider_library_version = '2.4.21';
+	$slider_frontend_js = plugin_dir_url( __FILE__ ) . 'js/slider-frontend.js';
+	$slider_frontend_version = '1.0.0';
 
-  global $post;
-  if( has_block( 'emma/slider', $post ) || emma_slider_has_block_in_reusable_block( 'emma/slider' ) ) {
-    wp_enqueue_script( 'slider-library', $slider_library_js, [], $slider_library_version, true );
-    wp_enqueue_style( 'slider-library', $slider_library_css, [], $slider_library_version );
-    
-    wp_enqueue_script( 'slider-frontend', $slider_frontend_js, ['slider-library'], $slider_frontend_version, true );
-  }
+	if ( has_block( 'emma/slider', $post ) || emma_slider_has_block_in_reusable_block( 'emma/slider' ) ) {
+		wp_enqueue_script( 'slider-library', $slider_library_js, [], $slider_library_version, true );
+		wp_enqueue_style( 'slider-library', $slider_library_css, [], $slider_library_version );
+		wp_enqueue_script( 'slider-frontend', $slider_frontend_js, ['slider-library'], $slider_frontend_version, true );
+	}
 }
 add_action( 'wp_enqueue_scripts', 'emma_slider_enqueue_frontend' );
 
@@ -35,35 +35,36 @@ add_action( 'wp_enqueue_scripts', 'emma_slider_enqueue_frontend' );
  * Register and enqueue editor scripts
  */
 function emma_slider_enqueue_editor() {
-  $slider_editor_js = plugin_dir_url( __FILE__ ) . 'js/slider-editor.js';
-  $slider_editor_version = '1.0.0';
+	$slider_editor_js = plugin_dir_url( __FILE__ ) . 'js/slider-editor.js';
+	$slider_editor_version = '1.0.0';
 
-  wp_enqueue_script( 'slider-editor', $slider_editor_js, ['wp-blocks','wp-editor'], $slider_editor_version, true );
+	wp_enqueue_script( 'slider-editor', $slider_editor_js, ['wp-blocks','wp-editor'], $slider_editor_version, true );
 }
 add_action( 'enqueue_block_editor_assets', 'emma_slider_enqueue_editor' );
 
-//needed because `has_block` does not detect blocks within reusable blocks, for some reason
+// Needed because `has_block` does not detect blocks within reusable blocks, for some reason.
 function emma_slider_has_block_in_reusable_block( $block_name, $id = false ){
-  $id = (!$id) ? get_the_ID() : $id;
-  if( $id ){
-      if ( has_block( 'block', $id ) ){
-          // Check reusable blocks
-          $content = get_post_field( 'post_content', $id );
-          $blocks = parse_blocks( $content );
+	$id = (!$id) ? get_the_ID() : $id;
 
-          if ( ! is_array( $blocks ) || empty( $blocks ) ) {
-              return false;
-          }
+	if ( $id ) {
+		if ( has_block( 'block', $id ) ){
+			// Check reusable blocks
+			$content = get_post_field( 'post_content', $id );
+			$blocks = parse_blocks( $content );
 
-          foreach ( $blocks as $block ) {
-              if ( $block['blockName'] === 'core/block' && ! empty( $block['attrs']['ref'] ) ) {
-                  if( has_block( $block_name, $block['attrs']['ref'] ) ){
-                     return true;
-                  }
-              }
-          }
-      }
-  }
+			if ( ! is_array( $blocks ) || empty( $blocks ) ) {
+				return false;
+			}
 
-  return false;
+			foreach ( $blocks as $block ) {
+				if ( $block['blockName'] === 'core/block' && ! empty( $block['attrs']['ref'] ) ) {
+					if ( has_block( $block_name, $block['attrs']['ref'] ) ){
+						return true;
+					}
+				}
+			}
+		}
+	}
+
+	return false;
 }
--- js/slider-frontend.js
+++ js/slider-frontend.js
@@ -1,29 +1,34 @@
 ( function() {
-  document.addEventListener( 'DOMContentLoaded', function () {
-		new Splide( '.splide' ).mount();
+	document.addEventListener( 'DOMContentLoaded', function () {
+		// Initialize sliders.
+		const sliders = document.querySelectorAll( '.splide' );
+
+		sliders.forEach( slider => {
+			new Splide( slider ).mount();
+		} );
 	} );
 
-  document.querySelectorAll( '.glide__main' ).forEach( item => {
-    var sliderSettings = JSON.parse( item.dataset.sliderSettings );
-    var slider = new Glide( item, sliderSettings ).mount();
+	document.querySelectorAll( '.glide__main' ).forEach( item => {
+		var sliderSettings = JSON.parse( item.dataset.sliderSettings );
+		var slider = new Glide( item, sliderSettings ).mount();
 
-    if( sliderSettings.itemWidth ) {
-      var minItemWidth = sliderSettings.itemWidth;
+		if( sliderSettings.itemWidth ) {
+			var minItemWidth = sliderSettings.itemWidth;
 
-      function setPerView() {
-        var maxSlides = Math.floor( item.clientWidth / minItemWidth );
-        var actualSlides = item.querySelectorAll( '.glide__slide:not(.glide__slide--clone)' ).length;
+			function setPerView() {
+				var maxSlides = Math.floor( item.clientWidth / minItemWidth );
+				var actualSlides = item.querySelectorAll( '.glide__slide:not(.glide__slide--clone)' ).length;
 
-        slider.update( {
-          perView: Math.min( maxSlides, actualSlides),
-        } );
-      }
+				slider.update( {
+					perView: Math.min( maxSlides, actualSlides),
+				} );
+			}
 
-      window.addEventListener( 'resize', function() {
-        setPerView();
-      } );
+			window.addEventListener( 'resize', function() {
+				setPerView();
+			} );
 
-      setPerView();
-    }
-  } );
+			setPerView();
+		}
+	} );
 } ) ();
